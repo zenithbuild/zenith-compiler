db7231181efdd02dc557af20183779d94fea99ab
Judah Sullivan <judahsullivan.dev@gmail.com>
2026-01-25 02:08:28 -0600

    chore: include all pending changes for v1.0.12 release

---
+++ native/compiler-native/target/debug/examples/repro-0271c46075ff7bec.d
@@ -0,0 +1,5 @@
+/Users/judahsullivan/Personal/zenith/zenith-compiler/native/compiler-native/target/debug/examples/repro-0271c46075ff7bec.d: examples/repro.rs
+
+/Users/judahsullivan/Personal/zenith/zenith-compiler/native/compiler-native/target/debug/examples/repro-0271c46075ff7bec: examples/repro.rs
+
+examples/repro.rs:
---
+++ native/compiler-native/target/debug/examples/repro.d
@@ -0,0 +1,1 @@
+/Users/judahsullivan/Personal/zenith/zenith-compiler/native/compiler-native/target/debug/examples/repro: /Users/judahsullivan/Personal/zenith/zenith-compiler/native/compiler-native/build.rs /Users/judahsullivan/Personal/zenith/zenith-compiler/native/compiler-native/examples/repro.rs
